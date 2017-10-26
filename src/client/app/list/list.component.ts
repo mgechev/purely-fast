f8c5cd0fdb036b47e656bb6d2fd035a20624c2d0
mgechev <mgechev@gmail.com>
2017-10-26 16:54:50 -0700

    Finalize list component

--- src/client/app/list/list.component.ts
+++ src/client/app/list/list.component.ts
@@ -13,24 +13,24 @@ const fibonacci = (num: number): number => {
   moduleId: module.id,
   selector: 'sd-list',
   template: `
-    <h1>{{ department }}</h1>
+    <h1 title="Department">{{ department }}</h1>
 
     <mat-form-field>
-      <input matInput type="text" [(ngModel)]="label" (keydown)="handleKey($event)">
+      <input placeholder="Enter name here" matInput type="text" [(ngModel)]="label" (keydown)="handleKey($event)">
     </mat-form-field>
 
     <mat-list>
       <div *ngIf="data.length === 0" class="empty-list-label">Empty list</div>
       <mat-list-item *ngFor="let item of data">
-        <h3 matLine>
+        <h3 matLine title="Name">
           {{ item.label }}
         </h3>
         <mat-chip-list>
-          <md-chip class="mat-chip mat-primary mat-chip-selected" color="primary" selected="true">
+          <md-chip title="Score" class="mat-chip mat-primary mat-chip-selected" color="primary" selected="true">
             {{ format(item.num) }}
           </md-chip>
         </mat-chip-list>
-        <i class="fa fa-trash-o" aria-hidden="true" (click)="remove.emit(item)"></i>
+        <i title="Delete" class="fa fa-trash-o" aria-hidden="true" (click)="remove.emit(item)"></i>
       </mat-list-item>
       <mat-divider *ngIf="data.length !== 0"></mat-divider>
     </mat-list>
