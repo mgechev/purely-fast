c11920821b98846f90c214d0b134d0f647ea1d94
mgechev <mgechev@gmail.com>
2017-10-29 16:24:49 -0700

    perf: add memoization

--- src/client/app/employee-list/format.pipe.ts
+++ src/client/app/employee-list/format.pipe.ts
@@ -1,11 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-const fibonacci = (num: number): number => {
+const memoize = require('lodash.memoize');
+
+const fibonacci = memoize((num: number): number => {
   if (num === 1 || num === 2) {
     return 1;
   }
   return fibonacci(num - 1) + fibonacci(num - 2);
-};
+});
 
 @Pipe({
   name: 'format'
