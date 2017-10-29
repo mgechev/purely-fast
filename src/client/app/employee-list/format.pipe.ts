9062bc486ed8a6073c0d934b1a149211241a6729
mgechev <mgechev@gmail.com>
2017-10-29 16:28:10 -0700

    style: add pure: true to be more explicit

--- src/client/app/employee-list/format.pipe.ts
+++ src/client/app/employee-list/format.pipe.ts
@@ -7,9 +7,7 @@ const fibonacci = (num: number): number => {
   return fibonacci(num - 1) + fibonacci(num - 2);
 };
 
-@Pipe({
-  name: 'format'
-})
+@Pipe({ name: 'format', pure: true })
 export class FormatPipe implements PipeTransform {
   transform(num: number) {
     return fibonacci(num);
