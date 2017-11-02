7fedad06ad0de834f9366d38f66f30b7277ed6df
mgechev <mgechev@gmail.com>
2017-11-01 19:14:40 -0700

    Refactoring

--- src/client/app/employee-list/employee-list.component.ts
+++ src/client/app/employee-list/employee-list.component.ts
@@ -29,7 +29,7 @@ const fibonacci = (num: number): number => {
         </h3>
         <mat-chip-list>
           <md-chip title="Score" class="mat-chip mat-primary mat-chip-selected" color="primary" selected="true">
-            {{ format(item.num) }}
+            {{ calculate(item.num) }}
           </md-chip>
         </mat-chip-list>
         <i title="Delete" class="fa fa-trash-o" aria-hidden="true" (click)="remove.emit(item)"></i>
@@ -55,7 +55,7 @@ export class EmployeeListComponent {
     }
   }
 
-  format(num: number) {
+  calculate(num: number) {
     return fibonacci(num);
   }
 }
